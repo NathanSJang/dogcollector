1da7fdf11aa0c694157d23bf25be93e5b6312fcc
Jang <sbtan1989@gmail.com>
2020-12-16 13:58:50 -0800

    first day of  django with SEI

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+
+# fake data
+class Dog:
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+
+dogs = [
+    Dog('Bell', 'Dobermann', 3),
+    Dog('Shaw', 'Terrier', 7),
+    Dog('Del', 'Poodle', 0),
+]
+
+
 # Create your views here.
 
 
@@ -9,3 +25,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {'dogs': dogs})
