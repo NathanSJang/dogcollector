e174c0f78a78db3448ce948f1718f704341a2227
Jang <sbtan1989@gmail.com>
2020-12-22 16:11:12 -0800

    fixed add dog

--- main_app/models.py
+++ main_app/models.py
@@ -19,7 +19,7 @@ class Toy(models.Model):
     color = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.Name
+        return self.name
 
     def get_absolute_url(self):
         return reverse('toys_detail', kwargs={'pk': self.id})
